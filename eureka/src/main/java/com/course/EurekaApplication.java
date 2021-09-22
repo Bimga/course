6fd68e8330882128e6dbaaf8b810d18c71e5a8da
86156 <96259815@qq.com>
2021-09-22 20:48:20 +0800

    初始化项目

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,4 +1,5 @@
 package com.course;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,12 +9,14 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaServer
-
 public class EurekaApplication {
+
     private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
-//    	public static void main(String[] args) {
+
+//	public static void main(String[] args) {
 //		SpringApplication.run(EurekaApplication.class, args);
 //	}
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(EurekaApplication.class);
         Environment env = app.run(args).getEnvironment();
@@ -21,3 +24,4 @@ public class EurekaApplication {
         LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 }
+
